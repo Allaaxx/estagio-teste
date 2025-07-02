8421953e40689ae997b12444258ca2794872a0d4
Allaaxx <allanrao358@gmail.com>
2025-07-02 16:59:54 -0300

    feat: add dotenv dependency and implement filmes database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { server } from './server/Server';
 
+
 const startServer = () => {
   server.listen(3333, () => {
-    console.log(`Servidor rodando em http://localhost:3333}`);
+    console.log(`Ctrl + Left click: http://localhost:3333`);
   });
 };
+
+startServer();
 
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
 const router = Router();
+const { filmes } = require("../database");
 
 router.get("/", (req, res) => {
   res.json({ message: "Welcome to the API!" });
 });
 
 router.get("/filmes", (req, res) => {
-  res.json({ status: "OK" });
+  res.json(filmes);
 });
 
 export { router };
